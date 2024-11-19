eb8e6a0ab83aae525692ecba9e7ffa802de9b471
Dzolowski <hubert00115@gmail.com>
2024-11-19 21:47:15 +0100

    Added tests

---
+++ README.rst
@@ -0,0 +1,34 @@
+.. image:: .assets/logo_gb.png
+   :alt: Logo GB
+   :width: 600
+   :allign: center
+
+Goodbyte devcontainers features repository
+##########################################
+
+
+Features list
+=============
+
+- **NCS_toolchain**
+
+- **JLink**
+
+NCS_toolchain
+*************
+
+The NCS_toolchain feature installs the complete toolchain required for developing applications within the Nordic Semiconductor ecosystem (NCS). It sets up all necessary tools and dependencies for building and debugging software for Nordic microcontrollers, streamlining the setup process for developers.
+
+JLink
+*************
+
+The JLink feature integrates SEGGER J-Link debugging tools into the development environment. It provides support for debugging and programming embedded systems, specifically enabling advanced capabilities for Nordic Semiconductor microcontrollers and other ARM-based devices. This feature ensures that the JLink software, including utilities like JFlashExe, is available and correctly configured for seamless debugging and flashing of firmware.
+
+References
+==========
+`<https://github.com/devcontainers/feature-starter>`_
+
+
+📜 License
+This project is licensed under MIT Licence same as the original project.
+
---
+++ src/NCS_toolchain/install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+NRFUTIL_URL="https://developer.nordicsemi.com/.pc-tools/nrfutil/x64-linux/nrfutil"
+NCLT_URL="https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-0/nrf-command-line-tools-10.24.0_linux-amd64.tar.gz"
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
+
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=NON_ROOT
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" > /dev/null 2>&1; then
+    USERNAME=root
+fi
+export USERNAME_VAR="${USERNAME}"
+
+wget -q "$NRFUTIL_URL" -O /usr/local/bin/nrfutil
+if [ $? -ne 0 ]; then
+    echo "Error: Downloading nrfutil failed!"
+    exit 1
+else
+    echo "nrfutil downloaded successfully."
+fi
+
+chmod +x /usr/local/bin/nrfutil
+
+
+sudo -u "${USERNAME}" nrfutil install nrf5sdk-tools
+sudo -u "${USERNAME}" nrfutil install device
+sudo -u "${USERNAME}" nrfutil install toolchain-manager
+
+rm -f ${USERNAME}/ncs/downloads/*
+
+sudo -u "${USERNAME}" nrfutil toolchain-manager install --ncs-version v${VERSION}
+if [ $? -ne 0 ]; then
+    echo "Error: Installing NCS version v${VERSION} failed!"
+    exit 1
+else
+    echo "NCS version v${VERSION} installed successfully."
+fi
+
+
+sudo -u "${USERNAME}" nrfutil toolchain-manager env --ncs-version v${VERSION} --as-script > /usr/local/bin/.zephyrrc
+
+
+if [ -n "$NCLT_URL" ]; then
+    TEMP_DIR=$(mktemp -d)
+    cd "$TEMP_DIR"
+    
+    wget -qO- "$NCLT_URL" | tar -xz || { echo "Download error"; exit 1; }
+    if [ -d ./nrf-command-line-tools ]; then
+        cp -r ./nrf-command-line-tools /opt
+        ln -sf /opt/nrf-command-line-tools/bin/nrfjprog /usr/local/bin/nrfjprog
+        ln -sf /opt/nrf-command-line-tools/bin/mergehex /usr/local/bin/mergehex
+    else
+        echo "nrf-command-line-tools not found!"
+    fi
+
+    cd ~
+    rm -rf "$TEMP_DIR"
+else
+    echo "Skipping nRF Command Line Tools installation (URL not provided)"
+fi
+
+
